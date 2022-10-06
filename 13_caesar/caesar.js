fde4a75cea0cc8b3bac56bf3ab0a3bf9baac7202
rahulbablu <bablu.kounjula@gmail.com>
2022-10-06 15:59:04 +0530

    changes in caesar

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,10 +1,28 @@
-const caesar = function(str, num) {
-   let strArray = str.split("");
-   let charArray = [];
-   for(let i =0; i< strArray.length; i++){
-       charArray = strArray.charCodeAt(i).push();
-   }
-};
-
-// Do not edit below this line
-module.exports = caesar;
+const caesar = function(string, shift) {
+    return string
+      .split("")
+      .map(char => shiftChar(char, shift))
+      .join("");
+  };
+  
+  const codeSet = code => (code < 97 ? 65 : 97);
+  
+  const mod = (n, m) => (n % m + m) % m;
+  
+  const shiftChar = (char, shift) => {
+    const code = char.charCodeAt();
+  
+    if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+      return String.fromCharCode(
+        mod(code + shift - codeSet(code), 26) + codeSet(code)
+      );
+    }
+    return char;
+  };
+  
+  module.exports = caesar;
+  Footer
+  © 2022 GitHub, Inc.
+  Footer navigation
+  Terms
+  
